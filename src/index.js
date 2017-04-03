0280feec767f0a3a4ecf31f892d04e5a997164df
Matthew Marcus <mmarcus@fanatics.com>
2017-04-03 12:10:33 -0700

    refactored a few things

--- src/index.js
+++ src/index.js
@@ -25,18 +25,22 @@ yargs.usage(`$0 -f {floorCount} -e {elevatorCount}`)
     t: {
       alias: `timeout`,
       number: true,
-      description: `Number of seconds to run the simulation for`
+      description: `Number of seconds to run the simulation for`,
+      default: 10
     }
   })
   .alias(`h`, `help`)
   .help()
-  .argv
 
 let elevators = []
 let floorPanels = []
 
-let elevatorCount = yargs.argv.elevators
-let floorCount = yargs.argv.floors
+const elevatorCount = yargs.argv.elevators
+const floorCount = yargs.argv.floors
+
+const simDuration = yargs.argv.timeout * 1000
+
+let simIsRunning = true
 
 for (let i = 1; i <= elevatorCount; i++) {
   elevators.push(new Elevator(i))
@@ -46,28 +50,34 @@ for (let f = 1; f <= floorCount; f++) {
   floorPanels.push(new FloorPanel(f, f === floorCount))
 }
 
-
-function getRandomDirection () {
-  return DIRECTIONS[Object.keys(DIRECTIONS)[Math.floor(Math.random() * 2)]]
+function getRandomInt (int) {
+  return Math.floor(Math.random() * int)
 }
 
-let intervalHandle = setInterval(() => {
-  const floorIndex = Math.floor(Math.random() * floorPanels.length)
-  const floorPanel = floorPanels[floorIndex]
-  console.info(`floor-${floorIndex + 1} Current panel state =>`, floorPanel.state)
-  let direction = getRandomDirection()
-
-  direction = (floorPanel.isLobby && direction === DIRECTIONS.DOWN) ?
-    DIRECTIONS.UP : direction
-  direction = (floorPanel.isTopFloor && direction === DIRECTIONS.UP) ?
-    DIRECTIONS.DOWN : direction
-
-  console.info(`floor-${floorIndex + 1} pushing button =>`, direction)
-  floorPanel.handleButtonPush(direction)
-}, 500)
-
 setTimeout(() => {
-    clearInterval(intervalHandle)
-  },
-  (yargs.argv.timeout || yargs.argv.timeout === 0) ? yargs.argv.timeout * 1000 : 10000
-)
+  simIsRunning = false
+}, simDuration)
+
+function initUser () {
+  if (simIsRunning) {
+    const floorIndex = Math.floor(Math.random() * floorPanels.length)
+    const floorPanel = floorPanels[floorIndex]
+    console.info(`floor-${floorIndex + 1} Current panel state =>`, floorPanel.state)
+    let direction = getRandomDirection()
+
+    direction = (floorPanel.isLobby && direction === DIRECTIONS.DOWN) ?
+      DIRECTIONS.UP : direction
+    direction = (floorPanel.isTopFloor && direction === DIRECTIONS.UP) ?
+      DIRECTIONS.DOWN : direction
+
+    console.info(`floor-${floorIndex + 1} pushing button =>`, direction)
+    floorPanel.handleButtonPush(direction)
+    setTimeout(initUser, getRandomInt(200) + 50)
+  }
+}
+
+initUser()
+
+function getRandomDirection () {
+  return DIRECTIONS[Object.keys(DIRECTIONS)[getRandomInt(2)]]
+}
